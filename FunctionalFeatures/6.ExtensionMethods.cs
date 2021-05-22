3d2fb857216628a3cb1ebac79dee8a3e31de0d5a
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-22 17:55:11 +0100

    Functional Features: (More) Extension Methods

--- FunctionalFeatures/6.ExtensionMethods.cs
+++ FunctionalFeatures/6.ExtensionMethods.cs
@@ -1,8 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FunctionalConcepts
 {
+    /// <summary>
+    /// Extension methods are a way to "add" methods to existing types without
+    /// creating a new derived type, recompiling, or otherwise modifying the
+    /// original type.
+    /// </summary>
     public static class ExtensionMethods
     {
         public static void Demonstrate()
@@ -10,14 +16,19 @@ namespace FunctionalConcepts
             var colors = new List<string> {"purple", "yellow", "orange"};
             colors.ShowItems();
 
-            var numbers = new[] {2, 4, 6, 8};
-            numbers.ShowItems();
+            if (!colors.IsEmpty())
+                Console.WriteLine("\nThe collection has items.");
+
+            Console.WriteLine("\n" + colors.GetRandomElement());
+
+            var shuffledColors = colors.Shuffle();
+            shuffledColors.ShowItems();
         }
     }
 
     static class CollectionExtensions
     {
-        private static readonly Random _Ran = new Random();
+        private static readonly Random _Ran = new();
 
         public static void ShowItems<T>(this IEnumerable<T> collection)
         {
@@ -25,5 +36,14 @@ namespace FunctionalConcepts
             foreach (T item in collection)
                 Console.WriteLine(item);
         }
+
+        public static bool IsEmpty<T>(this ICollection<T> candidateCollection) =>
+            candidateCollection == null || !candidateCollection.Any();
+
+        public static T GetRandomElement<T>(this ICollection<T> array) =>
+            array.ElementAt(_Ran.Next(array.Count));
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> candidate) =>
+            candidate.OrderBy(x => _Ran.NextDouble());
     }
 }
