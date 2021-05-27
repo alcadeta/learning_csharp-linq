8aaec8da9f73a2f8e6203bb1bdea4c1326ff0d58
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-27 09:28:57 +0100

    Aggregation: Common Aggregations

---
+++ Aggregation/Common.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using Filtering;
+
+namespace Aggregation
+{
+    public static class Common
+    {
+        public static void Demonstrate(List<int> numbers)
+        {
+            var total = numbers.Sum();
+            var median = numbers.Average();
+            var lowest = numbers.Min();
+            var highest = numbers.Max();
+            var countOf = numbers.Count();
+            var anotherCountOf = numbers.Count(a => a > 20);
+
+            total.Dump("Sum");
+            median.Dump("Average");
+            lowest.Dump("Min");
+            highest.Dump("Max");
+            countOf.Dump("Count");
+            anotherCountOf.Dump("Count with predicate");
+        }
+    }
+}
---
+++ Aggregation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Filtering;
+
+namespace Aggregation
+{
+    /// <summary>
+    /// Aggregate, given a sequence (a,b,c,d,e) perform an operation on each
+    /// successive pair
+    ///
+    /// var result = operation(a, b);
+    /// result = operation(result, c);
+    /// result = operation(result, d);
+    /// result = operation(result, e);
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> {50, 4, 10, 3, 90, 8, -60, 7, 20, 5};
+
+            Common.Demonstrate(numbers);
+        }
+    }
+}
